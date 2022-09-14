5fb2370a988d06d47861a55c186d4db695eff5ea
kavyabs11 <kavyasomshekar11@gmail.com>
2022-09-14 03:37:10 -0700

    demo

---
+++ Day8EmpWage/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Day8EmpWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("Welcome to employee wage computation program");
+            PorA emp = new PorA();
+            emp.Wage();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
