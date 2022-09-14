fc3a9c94e6ec55ded62c2c499a9c8767638e918f
kavyabs11 <kavyasomshekar11@gmail.com>
2022-09-14 07:09:11 -0700

    Dailywagecheck

---
+++ Day8EmpWage/EmpWage/DailyWage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage
+{
+    internal class DailyWage
+    {
+        public void Wage()
+        {
+            int fullTime = 1;
+            int empHr = 0;
+            int wagePerHr = 20;
+            int empWage = 0;
+            //new operator creates an object from the class by allocating memory for the new object and returning reference to thet memory
+            Random random = new Random();
+            int attendCheck = random.Next(0, 2);
+            if (attendCheck == fullTime)
+            {
+                empHr = 8;
+                Console.WriteLine($"The Employee is present");
+            }
+            else
+            {
+                empHr = 0;
+                Console.WriteLine($"The Employee wage is absent");
+            }
+            empWage = empHr * wagePerHr;
+            Console.WriteLine($"The Empployee wage is {empWage}");
+        }
+    }
+}
---
+++ Day8EmpWage/EmpWage/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EmpWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DailyWage dailywage = new DailyWage();
+            dailywage.Wage();
+        }
+    }
+}
