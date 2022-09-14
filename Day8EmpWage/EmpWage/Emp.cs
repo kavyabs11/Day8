f801b04ca8a1118c3e7487ca8388d4ae3ae96000
kavyabs11 <kavyasomshekar11@gmail.com>
2022-09-14 07:31:04 -0700

    limiting 20days nd 100hrs

--- Day8EmpWage/EmpWage/Emp.cs
+++ Day8EmpWage/EmpWage/Emp.cs
@@ -16,8 +16,10 @@ namespace EmpWage
             int empWage = 0;
             int wagePerHR = 20;
             int totalWorkingdays = 20;
-            int totalWage = 0;
-            for (int i = 0; i < totalWorkingdays; i++)
+            int totalWorkingHR = 100;
+            int totalEmpHr = 0;
+            int totalDay = 0;
+            do
             {
                 //new operator creates an object from the class by allocating memory for the new object and returning a reference to thet memory
                 Random random = new Random();
@@ -37,11 +39,14 @@ namespace EmpWage
                     empHr = 0;
                     Console.WriteLine("The employee is absent");
                 }
+                totalEmpHr = totalEmpHr + empHr;
                 empWage = empHr * wagePerHR;
                 Console.WriteLine($"Employee wage is {empWage}");
-                totalWage = totalWage + empWage;
-            }
-            Console.WriteLine($"Total wage for 20 working days is  {totalWage}");
+                totalDay++;
+            } while (totalEmpHr <= totalWorkingHR && totalDay < totalWorkingdays);
+            Console.WriteLine($"The total working days per month is {totalWorkingdays} and total working hours is {totalEmpHr}");
+            empWage = totalEmpHr * wagePerHR;
+            Console.WriteLine($"Employee wage is {empWage}");
         }
     }
 }
